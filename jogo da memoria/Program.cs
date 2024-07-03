0443afb35a3bef6c8307b1ad9b0fb97180728fdf
alfredohenriquedossantosbalsante <alfredobalsante@gmail.com>
2024-07-03 12:36:20 -0300

    gsdsshf

--- jogo da memoria/Program.cs
+++ jogo da memoria/Program.cs
@@ -1,148 +1,228 @@
-﻿using System.Diagnostics;
+﻿
+using System;
+using System.Runtime.CompilerServices;
 
-namespace jogo_da_memoria
+namespace Matriz2024
 {
     internal class Program
     {
-        static void PrintMatrix(int[,] tela)
+        static void PrintMatrix(int[,] screen)
         {
+            Console.Write("   ");
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write(" {0}  ", i);
+            }
+            Console.WriteLine("\n  -----------------");
+            //Impressão
             for (int j = 0; j < 4; j++)
             {
+                Console.Write("{0} |", j + 1);
                 for (int k = 0; k < 4; k++)
                 {
-                    Console.Write(tela[j, k]);
+                    Console.Write(" {0} |", screen[j, k]);
                 }
-                Console.WriteLine();
+                Console.WriteLine("\n  -----------------");
+
             }
             Console.WriteLine();
         }
-static void Main(string[] args)
+        static void Main(String[] Args)
         {
             int[,] tela = new int[4, 4];
             int[,] jogo = new int[4, 4];
             int acertos = 0;
-            int jogadasp1 = 0, jogadasp2 = 0;
+            Player p1;
+            Player p2;
+            int jogadas = 0;
 
-            Console.WriteLine("entre com o nome do player 1: ");
-            string namep1 = Console.ReadLine();
-            Console.WriteLine("entre com o nome do player 2: ");
-            string namep2 = Console.ReadLine();
+            Console.WriteLine("Entre com o nome do P1:");
+            string nameP1 = Console.ReadLine();
+            Console.WriteLine("Entre com o nome do P2:");
+            string nameP2 = Console.ReadLine();
 
-            player p1 = new player(namep1);
-            player p2 = new player(namep2);
-
-            Console.WriteLine("nome: "+ p1.Name);
-            Console.WriteLine("pontuação: " + p1.Score);
-
-            Console.WriteLine(p1._name);
-            Console.WriteLine(p2._name);
+            p1 = new Player(nameP1);
+            p2 = new Player(nameP2);
 
 
-            Environment.Exit(0);
 
-            int linha, coluna;
 
             Random gerador = new Random();
 
+            int lin, col;
+            //Vamos gerar os pares de números e adicionar
+            //na matriz jogo
             for (int i = 1; i <= 8; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int n = 0; n < 2; n++)
                 {
+                    do
+                    {
+                        lin = gerador.Next(0, 4);
+                        col = gerador.Next(0, 4);
+                    } while (jogo[lin, col] != 0);
 
+                    jogo[lin, col] = i;
+                }
+            }
+            //Sortear jogador que começa
+            int jogador = gerador.Next(1, 3);
+            Program.PrintMatrix(jogo);
+            Console.WriteLine();
+            do
+            {
+                //Imprimir o nome do jogador da vez.
+                //if(jogador == 1)
+                //    Console.WriteLine(p1.Name);
+                //else
+                //    Console.WriteLine(p2.Name);
+
+                Console.WriteLine("{0} É A SUA VEZ!",
+                    jogador == 1 ? p1.Name : p2.Name);
+
+                //Começa a contar o tempo.
+                DateTime begin = DateTime.Now;
+
+                //Imprimir conteúdo da matriz tela
+                Program.PrintMatrix(tela);
+
+                int lin1, col1;
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("Entre com a linha [1, 4]");
+                        lin1 = int.Parse(Console.ReadLine());
+                    } while (lin1 <= 0 || lin1 >= 5);
 
                     do
                     {
-                        linha = gerador.Next(0, 4);
-                        coluna = gerador.Next(0, 4);
-                    } while (jogo[linha, coluna] != 0);
-                    jogo[linha, coluna] = i;
-                }
+                        Console.WriteLine("Entre com a coluna [1, 4]");
+                        col1 = int.Parse(Console.ReadLine());
+                    } while (col1 <= 0 || col1 >= 5);
 
-            }
-
-            int jogador = gerador.Next(1, 3);
-
-            
-            if(jogador == 1)
-            {
-                Console.WriteLine(p1.Name+" é a sua vez.");
-            }
-            else
-            {
-                Console.WriteLine(p2.Name+" é a sua vez."); 
-            }
-            DateTime begin = DateTime.Now;
-            do
-            {
+                    lin1--; //diminuir, pois o usuário não sabe
+                    col1--; //que existe linha e coluna zero
+                            //Atribuir o valor da matriz jogo na tela
+                    if (tela[lin1, col1] != 0)
+                        Console.WriteLine("ERRO: VOCÊ JÁ ESCOLHEU ESTA POSIÇÃO!");
+                } while (tela[lin1, col1] != 0);
 
 
-                Program.PrintMatrix(tela);
-                    int linha1, coluna1;
-                Console.WriteLine("entre com a linha[1, 4]");
-                linha1 = int.Parse(Console.ReadLine());
-                Console.WriteLine("entre com a coluna[1, 4]");
-                coluna1 = int.Parse(Console.ReadLine());
+                tela[lin1, col1] = jogo[lin1, col1];
 
-                linha1--;
-                coluna1--;
-                tela[linha1, coluna1] = jogo[linha1, coluna1];
-
-               Program.PrintMatrix(tela);
-                int linha2, coluna2;
-                Console.WriteLine("entre com a linha[1, 4]");
-                linha2 = int.Parse(Console.ReadLine());
-                Console.WriteLine("entre com a coluna[1, 4]");
-                coluna2 = int.Parse(Console.ReadLine());
-
-                linha2--;
-                coluna2--;
-                tela[linha2, coluna2] = jogo[linha2, coluna2];
-               Program.PrintMatrix(tela);
-
-                if (tela[linha1, coluna1] == tela[linha2, coluna2])
+                Console.Write("   ");
+                for (int i = 1; i <= 4; i++)
                 {
-                    if(jogador == 1)
-                    {
-                        p1.Score = 1;
-                        jogadasp1++;
-                    }
-                    else
-                    {
-                        p2.Score = 1;
-                        jogadasp2++;
-                    }
-                    acertos++;
-                    
+                    Console.Write(" {0}  ", i);
                 }
-                else
+                Console.WriteLine("\n  -----------------");
+                //Impressão
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("{0} |", j + 1);
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write(" {0} |", tela[j, k]);
+                    }
+                    Console.WriteLine("\n  -----------------");
+
+                }
+                Console.WriteLine();
+
+                int lin2, col2;
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("Entre com a linha [1, 4]");
+                        lin2 = int.Parse(Console.ReadLine());
+                    } while (lin2 <= 0 || lin2 >= 5);
+                    do
+                    {
+                        Console.WriteLine("Entre com a coluna [1, 4]");
+                        col2 = int.Parse(Console.ReadLine());
+                    } while (col2 <= 0 || col2 >= 5);
+
+                    lin2--; //diminuir, pois o usuário não sabe
+                    col2--; //que existe linha e coluna zero
+                            //Atribuir o valor da matriz jogo na tela
+                    if (tela[lin2, col2] != 0)
+                        Console.WriteLine("ERRO: VOCÊ JÁ ESCOLHEU ESTA POSIÇÃO!");
+                } while (tela[lin2, col2] != 0);
+
+                tela[lin2, col2] = jogo[lin2, col2];
+
+                Console.Write("   ");
+                for (int i = 1; i <= 4; i++)
+                {
+                    Console.Write(" {0}  ", i);
+                }
+                Console.WriteLine("\n  -----------------");
+                //Impressão
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("{0} |", j + 1);
+                    for (int k = 0; k < 4; k++)
+                    {
+                        Console.Write(" {0} |", tela[j, k]);
+                    }
+                    Console.WriteLine("\n  -----------------");
+
+                }
+                Console.WriteLine();
+
+                if (tela[lin1, col1] == tela[lin2, col2])
+                {
+                    if (jogador == 1)
+                        p1.Score = 1;
+                    else
+                        p2.Score = 1;
+
+                    if (jogador == 1)
+                        p1.Jogadas = 1;
+                    else
+                        p2.Jogadas = 1;
+
+                    jogadas++;
+
+                    acertos++;
+                }
+                else //Caso não tenha acertado o par
                 {
                     TimeSpan timeSpan = DateTime.Now - begin;
-
-                    if(jogador == 1)
-                    {
-                        p1.GameTime = timeSpan;
-                        jogadasp1++;
-                    }
+                    if (jogador == 1)
+                        p1.Jogadas = 1;
                     else
-                    {
+                        p2.Jogadas = 1;
+                    jogadas++;
+
+                    //Soma do tempo de partida.
+                    if (jogador == 1)
+                        p1.GameTime = timeSpan;
+                    else
                         p2.GameTime = timeSpan;
-                        jogadasp2++;
-                    }
-                    
-                    tela[linha1, coluna1] = 0;
-                    tela[linha2, coluna2] = 0;
+
+                    //Inversão dos jogadores
+                    jogador = jogador % 2 + 1;
+
+                    tela[lin1, col1] = 0;
+                    tela[lin2, col2] = 0;
                 }
+                //Console.WriteLine("Digite 0 para sair ou outro número para continuar:");
+                //int valor = int.Parse(Console.ReadLine());
 
-                
+                //if (valor == 0)
+                //    break;
 
-                
+                Console.WriteLine(p1.ToString());
+                Console.WriteLine(p2.ToString());
             } while (acertos < 8);
-            Console.WriteLine(p1.ToString());
-            Console.WriteLine(p2.ToString());
-            Console.WriteLine("o numero de jogadas do player 1 foi: " + jogadasp1); 
-            Console.WriteLine("o numero de jogadas do player 2 foi: " + jogadasp2);
 
 
+
+
+            //Console.WriteLine($"Tempo total de jogo: {timeSpan.ToString(@"hh\:mm\:ss")}");
 
         }
     }
--- jogo da memoria/player.cs
+++ jogo da memoria/player.cs
@@ -3,28 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
-namespace jogo_da_memoria
+namespace Matriz2024
 {
-    internal class player
+    internal class Player
     {
-        //atributos
-        public string _name;
-        int _score;
-        TimeSpan _gameTime;
-        public player(string name)
+        //Atributos
+        private string _name;
+        private int _score;
+        private TimeSpan _gameTime;
+        private int _jogadas;
+
+        public Player(string name)
         {
             Name = name;
             Score = 0;
             GameTime = TimeSpan.FromSeconds(0);
+            Jogadas = 0;
         }
+
         public string Name
         {
             get { return _name; }
             set
             {
-                if (string.IsNullOrEmpty(value))
+                if (!string.IsNullOrEmpty(value))
                     _name = value;
             }
         }
@@ -38,21 +41,31 @@ namespace jogo_da_memoria
                     _score += value;
             }
         }
+
         public TimeSpan GameTime
         {
             get { return _gameTime; }
             set
             {
-                _gameTime += value; 
+                _gameTime += value;
             }
         }
+
+        public int Jogadas
+        {
+            get { return _jogadas; }
+            set
+            {
+                _jogadas += value;
+            }
+        }
+
         public override string ToString()
         {
             return "Nome: " + Name +
-                "\nPontuação: " + Score +
-                $"\nTempo de partida:  {GameTime:hh\\:mm\\:ss}";
-
+            "\nPontuação: " + Score +
+            $"\nTempo de Partida: {GameTime:hh\\:mm\\:ss}" +
+            "\njogadas: " + Jogadas;
         }
-        
     }
 }
